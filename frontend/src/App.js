083d1bd104e0d377e54ec5f03c60d4cfdbd66c81
Rininmmmment <rinr10693otes@gmail.com>
2023-11-27 23:41:03 +0900

    add: プレイ画面のフロント

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,10 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Menu from './components/Menu';
 import Make from './components/Make';
+import Select from './components/Select';
+import Chapter from './components/Chapter';
+import Section from './components/Section';
+import Result from './components/Result';
 
 function App() {
   return (
@@ -12,6 +16,10 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/menu" element={<Menu />} />
           <Route path="/make" element={<Make />} />
+          <Route path="/play/select" element={<Select />} />
+          <Route path="/play/chapter" element={<Chapter />} />
+          <Route path="/play/section" element={<Section />} />
+          <Route path="/play/result" element={<Result />} />
         </Routes>
       </div>
     </BrowserRouter>
