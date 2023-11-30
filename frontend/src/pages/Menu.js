9ca860a8dfbd05edfdc14d5407b616f21c07bbe2
Rininmmmment <rinr10693otes@gmail.com>
2023-11-30 23:12:53 +0900

    add: メニュー画面UI

--- frontend/src/pages/Menu.js
+++ frontend/src/pages/Menu.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import '../css/Menu.scss';
 
 const Menu = () => {
   return (
-    <div>
-        <a href="#">MAKE AND WDIT QUIZ</a>
-        <a href="#">PLAY CODE</a>
-        <a href="#">SETTINGS</a>
+    <div class="menu-container">
+      <div class="centering-container">
+        <div class="make-play-container">
+          <a href="#" class="menu-btn make">MAKE AND EDIT<br />QUIZ</a>
+          <a href="#" class="menu-btn play">PLAY CODE</a>
+        </div>
+        <a href="#" class="menu-btn settings">SETTINGS</a>
+      </div>
     </div>
   );
 };
