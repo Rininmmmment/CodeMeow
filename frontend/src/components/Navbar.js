45d232bf9153380c2469c6d9d709a80ff17d865b
Rininmmmment <rinr10693otes@gmail.com>
2023-12-18 13:55:19 +0900

    add: ログアウトボタン

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 import '../css/Navbar.scss';
+import { useAuth } from '../components/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+  const { userInfo, logout } = useAuth();
+  const [userId, setUserId] = useState(userInfo);
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    logout();
+    navigate('/login');
+  };
+
   return (
     <nav>
       <a href="/menu" className="logo"><img src={process.env.PUBLIC_URL + '/images/logo_small.png'} className="logo" /></a>
-      <a href="#" className='user'>Log out</a>
+      <a href="" className='user' onClick={() => handleLogout(userId)}>Log out</a>
     </nav>
   );
 };
