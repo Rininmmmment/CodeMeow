056301a0a9f3c4e626ee738cf6476526bd3ba182
Rininmmmment <rinr10693otes@gmail.com>
2024-01-05 18:52:39 +0900

    add: 更新情報表示

--- frontend/src/pages/Menu.js
+++ frontend/src/pages/Menu.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../css/Menu.scss';
 import Navbar from '../components/Navbar.js';
 
 const Menu = () => {
+  const apiUrl = process.env.REACT_APP_API_URL;
+  const [updateInfos, setUpdateInfos] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${apiUrl}/updates`);
+        const data = await response.json();
+        setUpdateInfos(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="menu-container">
       <Navbar />
@@ -11,7 +27,16 @@ const Menu = () => {
           <a href="/make/upload" className="menu-btn make">MAKE QUIZZES</a>
           <a href="/code-list" className="menu-btn play">PLAY CODE</a>
         </div>
-        {/* <a href="#" className="menu-btn settings">SETTINGS</a> */}
+        <div className='update-infos-container'>
+          <h2>Information</h2>
+          {updateInfos.map(info => (
+            <div key={info.id} className='info-container'>
+              <h3>{info.title}</h3>
+              <p className='date'>{info.created_at}</p>
+              <p>{info.content}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
