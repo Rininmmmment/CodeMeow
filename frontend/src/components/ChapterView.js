970cf414b2957c1db8f549247634e35247ef47ac
Rininmmmment <rinr10693otes@gmail.com>
2024-01-20 23:49:00 +0900

    fix: デザインの調整

--- frontend/src/components/ChapterView.js
+++ frontend/src/components/ChapterView.js
@@ -101,13 +101,15 @@ const ChapterView = (props) => {
           {expandedSections.includes(index) && (
             <div className='code-container'>
               {sec?.quizzes.map((sq, innerIndex) => (
-                <div id={sq[2]} key={innerIndex}>
+                <div id={sq[2]} key={innerIndex} className='each-sq'>
                   <h3>{innerIndex + 1}: {sq[0]}</h3>
-                  <div className="code-toolbar">
-                    <button className='delete-btn' onClick={() => handleDelete(sq[2])}></button>
+                  <div className='code'>
+                    <div className="code-toolbar">
+                      <button className='delete-btn' onClick={() => handleDelete(sq[2])}></button>
+                    </div>
+                    {sq[1].length !== 0 && <div className='md-container'><ReactMarkdown>{sq[1]}</ReactMarkdown></div>}
+                    <CodeBlock code={sq[2]} />
                   </div>
-                  {sq[1].length !== 0 && <div className='md-container'><ReactMarkdown>{sq[1]}</ReactMarkdown></div>}
-                  <CodeBlock code={sq[2]} />
                 </div>
               ))}
             </div>
